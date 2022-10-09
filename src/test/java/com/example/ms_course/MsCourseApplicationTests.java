b5cdf9a63d9fd77a680c4f94ed1b88cd9041a26b
Shalale <selale.memmedli11@gmail.com>
2022-10-10 00:22:54 +0400

    docker integration

---
+++ src/test/java/com/example/ms_course/MsCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
