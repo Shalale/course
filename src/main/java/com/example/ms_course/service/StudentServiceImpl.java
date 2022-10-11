63d2c7ef123ee8d353606dc5eb94b53fc2be3590
Shalale <selale.memmedli11@gmail.com>
2022-10-11 13:12:35 +0400

    new functionalities

--- src/main/java/com/example/ms_course/service/StudentServiceImpl.java
+++ src/main/java/com/example/ms_course/service/StudentServiceImpl.java
@@ -17,41 +17,36 @@ public class StudentServiceImpl implements StudentService {
     private final StudentRepository repository;
     private final ModelMapper mapper;
 
-//    @Override
-//    public StudentDto getById(Long id) {
-//        Student student = repository.findById(id);
-//        return mapper.map(student, StudentDto.class);
-//    }
+    @Override
+    public StudentDto getById(Long id) {
+        Student student = repository.findById(id).orElseThrow();
+        return mapper.map(student, StudentDto.class);
+    }
 
-
-//    @Override
-//    public StudentDto createStudent(StudentDto studentDto) {
-//        Student student = mapper.map(studentDto, Student.class);
-//        repository.save(student);
-//        return studentDto;
-//    }
 
     @Override
     public StudentDto createStudent(StudentDto studentDto) {
-
+        Student student = mapper.map(studentDto, Student.class);
+        repository.save(student);
         return studentDto;
     }
 
-//    @Override
-//    public StudentDto updateStudent(Long id, StudentDto dto){
-//        Student student = repository.findById(id).orElseThrow();
-//
-//        student.setName(dto.getName());
-//        student.setSurname(dto.getSurname());
-//        student.setAge(dto.getAge());
-//
-//        repository.save(student);
-//        return mapper.map(student, StudentDto.class);
-//    }
-//
-//    @Override
-//    public void deleteStudent(Long id) {
-//        repository.findById(id).orElseThrow();
-//        repository.deleteById(id);
-//    }
+
+    @Override
+    public StudentDto updateStudent(Long id, StudentDto dto){
+        Student student = repository.findById(id).orElseThrow();
+
+        student.setName(dto.getName());
+        student.setSurname(dto.getSurname());
+        student.setAge(dto.getAge());
+
+        repository.save(student);
+        return mapper.map(student, StudentDto.class);
+    }
+
+    @Override
+    public void deleteStudent(Long id) {
+        repository.findById(id).orElseThrow();
+        repository.deleteById(id);
+    }
 }
